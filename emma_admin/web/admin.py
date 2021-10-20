438cb611c323620885f5c0354234d142e56087bc
Anton Pirker <anton@ignaz.at>
2021-10-20 16:58:47 +0200

    Added basic admin interface

--- emma_admin/web/admin.py
+++ emma_admin/web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from web.models import Booking, Customer, Unit
+
+admin.site.register(Unit)
+admin.site.register(Customer)
+admin.site.register(Booking)
