6fffc39a81042299ae009fdbc71d47fb2387ad3d
Anton Pirker <anton@ignaz.at>
2021-10-21 10:20:35 +0200

    Added admin for customer

--- emma_admin/web/admin.py
+++ emma_admin/web/admin.py
@@ -8,5 +8,43 @@ class UnitAdmin(admin.ModelAdmin):
 
 admin.site.register(Unit, UnitAdmin)
 
-admin.site.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        'last_name', 'first_name', 'birth_date',
+        'nationality', 'country_code', 'city',
+        'phone', 'email',
+        'comment', 'customer_comment',
+        )
+    list_filter = (
+        'nationality', 'country_code', 'city', 'preferred_language',
+    )
+    fieldsets = (
+        (None, {
+            'fields': (
+                'last_name', 'first_name', 'birth_date', 'nationality',
+            )
+        }),
+        ('Contact', {
+            'fields': (
+                'email', 'phone', 'preferred_language',
+            )
+        }),
+
+        ('Address', {
+            'fields': (
+                'address_line_1', 'address_line_2', 'postal_code', 'city',
+                'country_code',
+            )
+        }),
+        ('Comments', {
+            'fields': (
+                'comment', 'customer_comment',
+            ),
+        }),
+    )
+
+
+admin.site.register(Customer, CustomerAdmin)
+
+
 admin.site.register(Booking)
